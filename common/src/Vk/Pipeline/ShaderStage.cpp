c528c4f08baaeb1a6e6f0eaf235b4c256b3683c7
Adrien LENGLET <adrien.lenglet3@gmail.com>
2020-03-07 10:37:12 +0100

    Add ShaderStageCreateInfo.cpp

--- common/src/Vk/Pipeline/ShaderStage.cpp
+++ common/src/Vk/Pipeline/ShaderStage.cpp
@@ -29,34 +29,4 @@ Pipeline::ShaderStage::~ShaderStage(void)
 {
 }
 
-Pipeline::ShaderStageCreateInfo::ShaderStageCreateInfo(VkShaderStageFlagBits stage, std::string path, std::string entryPoint) :
-	stage(stage),
-	path(path),
-	entryPoint(entryPoint)
-{
-}
-
-Pipeline::ShaderStageCreateInfo::~ShaderStageCreateInfo(void)
-{
-}
-
-
-
-Pipeline::ShaderStages::ShaderStages(VkDevice dev, std::vector<ShaderStageCreateInfo> &createInfos) :
-	createInfos(getCreateInfos(dev, createInfos))
-{
-}
-
-std::vector<VkPipelineShaderStageCreateInfo> Pipeline::ShaderStages::getCreateInfos(VkDevice dev, std::vector<ShaderStageCreateInfo> &createInfos)
-{
-	std::vector<VkPipelineShaderStageCreateInfo> res;
-
-	reserve(createInfos.size());
-	for (auto &createInfo : createInfos)
-		emplace_back(dev, createInfo.stage, createInfo.path, createInfo.entryPoint);
-	for (auto &stage : *this)
-		res.push_back(stage.createInfo);
-	return res;
-}
-
 }
