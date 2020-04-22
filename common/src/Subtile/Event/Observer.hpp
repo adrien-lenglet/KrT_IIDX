83cc15e451397ecd78a23f60ccc5f78ed34276f7
Adrien LENGLET <adrien.lenglet3@gmail.com>
2020-04-22 08:41:57 +0200

    Add Event::Bindings

--- common/src/Subtile/Event/Observer.hpp
+++ common/src/Subtile/Event/Observer.hpp
@@ -101,6 +101,83 @@ private:
 
 class Socket;
 
+template <typename Key, typename Value>
+class Bindings
+{
+public:
+	using MapType = std::map<Key, std::map<Value*, std::unique_ptr<Value>>>;
+
+	Bindings(void)
+	{
+	}
+	~Bindings(void)
+	{
+	}
+
+	class Binder : Event::Listener
+	{
+	public:
+		Binder(Bindings &bindings, typename MapType::value_type &pair, Value &value) :
+			m_bindings(&bindings),
+			m_pair(&pair),
+			m_value(&value)
+		{
+		}
+		~Binder(void) override
+		{
+			if (m_bindings)
+				m_bindings->unbind(*m_pair, *m_value);
+		}
+
+		Binder(Binder &&other) :
+			m_bindings(other.m_bindings),
+			m_pair(other.m_pair),
+			m_value(other.m_value)
+		{
+			other.m_bindings = nullptr;
+			other.m_pair = nullptr;
+			other.m_value = nullptr;
+		}
+
+	private:
+		Bindings *m_bindings;
+		typename MapType::value_type *m_pair;
+		Value *m_value;
+	};
+
+	template <typename ...ArgsTypes>
+	Binder bind(const Key &key, ArgsTypes &&...args)
+	{
+		auto new_value = new Value(std::forward<ArgsTypes>(args)...);
+		auto res = m_bindings[key].emplace(new_value, std::unique_ptr(new_value));
+		if (!res.second)
+			throw std::runtime_error("Can't bind element in collection");
+		auto &p = *res.first;
+		return Binder(*this, p, *new_value);
+	}
+
+private:
+	friend Binder;
+	MapType m_bindings;
+
+	void unbind(typename MapType::value_type &pair, Value &value)
+	{
+		auto &map = pair.second;
+		auto got = map.find(&value);
+		if (got != map.end()) {
+			map.erase(got);
+			if (map.size() == 0) {
+				auto b_got = m_bindings.find(pair.first);
+				if (b_got != m_bindings.end())
+					m_bindings.erase(b_got);
+				else
+					throw std::runtime_error("Can't unbind");
+			}
+		} else
+			throw std::runtime_error("Can't unbind");
+	}
+};
+
 template <typename ObserverType, template <typename...> class GroupingType, typename... RequestTypes, typename... StoreTypes, typename... ReturnTypes>
 class Observer::Group<ObserverType, GroupingType<RequestTypes...>, GroupingType<StoreTypes...>, GroupingType<ReturnTypes...>> : public Observer
 {
