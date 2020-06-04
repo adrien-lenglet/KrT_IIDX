855fcce20b641b295db212488ac8f8b1727e0034
Adrien LENGLET <adrien.lenglet3@gmail.com>
2020-06-04 09:13:25 +0200

    Use scc for Transform

--- common/src/Subtile/Transform.cpp
+++ common/src/Subtile/Transform.cpp
@@ -1,100 +1,103 @@
 #include "Transform.hpp"
 #include "Entity.hpp"
 #include "Math/Quaternion.hpp"
+#include "Subtile/scp.hpp"
 
 namespace Subtile {
 
-template <>
-Transform<Entity>::Transform(void) :
-	scale(1.0),
-	rot(1.0, 0.0, 0.0, 0.0),
-	pos(0.0),
-	m_local(1.0),
-	m_world(1.0),
-	m_is_local_up(true),
-	m_is_world_up(true),
-	m_is_absolute(false)
-{
-}
+rt_scc(Transform<Entity>,
+	template <>
+	scp::Transform(void) :
+		scale(1.0),
+		rot(1.0, 0.0, 0.0, 0.0),
+		pos(0.0),
+		m_local(1.0),
+		m_world(1.0),
+		m_is_local_up(true),
+		m_is_world_up(true),
+		m_is_absolute(false)
+	{
+	}
 
-template <>
-Transform<Entity>::~Transform(void)
-{
-}
+	template <>
+	scp::~Transform(void)
+	{
+	}
 
-template <>
-Entity& Transform<Entity>::getFinal(void)
-{
-	return static_cast<Entity&>(*this);
-}
+	template <>
+	Entity& scp::getFinal(void)
+	{
+		return static_cast<Entity&>(*this);
+	}
 
-template <>
-void Transform<Entity>::updateLocal(void);
-template <>
-void Transform<Entity>::updateWorld(void);
+	template <>
+	void scp::updateLocal(void);
+	template <>
+	void scp::updateWorld(void);
 
-template <>
-mat4& Transform<Entity>::local(void)
-{
-	if (!m_is_local_up)
-		updateLocal();
-	return m_local;
-}
+	template <>
+	mat4& scp::local(void)
+	{
+		if (!m_is_local_up)
+			updateLocal();
+		return m_local;
+	}
 
-template <>
-mat4& Transform<Entity>::model_world(void)
-{
-	if (!m_is_local_up || !m_is_world_up)
-		updateWorld();
-	return m_world;
-}
+	template <>
+	mat4& scp::model_world(void)
+	{
+		if (!m_is_local_up || !m_is_world_up)
+			updateWorld();
+		return m_world;
+	}
 
-template <>
-void Transform<Entity>::parentMoved(void);
+	template <>
+	void scp::parentMoved(void);
 
-template <>
-void Transform<Entity>::transformed(void)
-{
-	m_is_local_up = false;
-	for (auto &c : getFinal().m_children)
-		c.parentMoved();
-}
+	template <>
+	void scp::transformed(void)
+	{
+		m_is_local_up = false;
+		for (auto &c : getFinal().m_children)
+			c.parentMoved();
+	}
 
-template <>
-void Transform<Entity>::updateLocal(void)
-{
-	m_local = static_cast<mat4>(rot);
-	m_local = math::scale(m_local, scale);
-	m_local = math::translate(m_local, pos);
-	m_is_local_up = true;
-}
+	template <>
+	void scp::updateLocal(void)
+	{
+		m_local = static_cast<mat4>(rot);
+		m_local = math::scale(m_local, scale);
+		m_local = math::translate(m_local, pos);
+		m_is_local_up = true;
+	}
 
-template <>
-void Transform<Entity>::updateWorld(void)
-{
-	auto &p = getFinal().m_parent;
+	template <>
+	void scp::updateWorld(void)
+	{
+		auto &p = getFinal().m_parent;
 
-	if (p && !m_is_absolute)
-		m_world = p->model_world() * local();
-	else
-		m_world = local();
-	m_is_world_up = true;
-}
+		if (p && !m_is_absolute)
+			m_world = p->model_world() * local();
+		else
+			m_world = local();
+		m_is_world_up = true;
+	}
 
-template <>
-void Transform<Entity>::setAbsolute(void)
-{
-	m_is_absolute = true;
-}
+	template <>
+	void scp::setAbsolute(void)
+	{
+		m_is_absolute = true;
+	}
 
-template <>
-void Transform<Entity>::parentMoved(void)
-{
-	if (m_is_absolute)
-		return;
-	m_is_world_up = false;
-	for (auto &c : getFinal().m_children)
-		c.parentMoved();
-}
+	template <>
+	void scp::parentMoved(void)
+	{
+		if (m_is_absolute)
+			return;
+		m_is_world_up = false;
+		for (auto &c : getFinal().m_children)
+			c.parentMoved();
+	}
+)
 
 }
